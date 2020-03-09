5f943e9d42f99554e526247a6b5ea4b8bb1b33ca
bannified <freedomzelda_dc@hotmail.com>
2020-03-10 00:34:34 +0800

    Initial Commit

---
+++ Assignment 2/Lab 01/mesh.cpp
@@ -0,0 +1,149 @@
+#include "mesh.h"
+
+#ifdef _WIN32
+#include <Windows.h>
+#include "GL\glut.h"
+#define M_PI 3.141592654
+#elif __APPLE__
+#include <OpenGL/gl.h>
+#include <GLUT/GLUT.h>
+#endif
+
+#include "math.h"
+#include <string>
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "mesh.h"
+#include <map>
+#include <queue>
+#include <iomanip>
+using namespace std;
+
+
+
+void myObjType::draw() {
+
+	glEnable(GL_LIGHTING);
+
+	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
+
+	glPushMatrix();
+	double longestSide = 0.0;
+	for (int i = 0; i < 3; i++)
+		if ((lmax[i] - lmin[i]) > longestSide)
+			longestSide = (lmax[i] - lmin[i]);
+	glScalef(4.0 / longestSide, 4.0 / longestSide, 4.0 / longestSide);
+	glTranslated(-(lmin[0] + lmax[0]) / 2.0, -(lmin[1] + lmax[1]) / 2.0, -(lmin[2] + lmax[2]) / 2.0);
+	for (int i = 1; i <= tcount; i++)
+	{
+		glBegin(GL_POLYGON);
+// uncomment the following after you computed the normals
+//		glNormal3dv(nlist[i]);    
+		for (int j = 0; j < 3; j++)
+			glVertex3dv(vlist[tlist[i][j]]);
+		glEnd();
+	
+	}
+	glDisable(GL_LIGHTING);
+
+	glPopMatrix();
+}
+
+void myObjType::writeFile(char* filename)
+{
+	}
+
+void myObjType::readFile(char* filename)
+{
+	cout << "Opening " << filename << endl;
+	ifstream inFile;
+	inFile.open(filename);
+	if (!inFile.is_open()) {
+		cout << "We cannot find your file " << filename << endl;
+		exit(1);
+	}
+
+	string line;
+	int i, j;
+	bool firstVertex = 1;
+	double currCood;
+
+	while (getline(inFile, line))
+	{
+		if ((line[0] == 'v' || line[0] == 'f') && line[1] == ' ')
+		{
+			if (line[0] == 'v')
+			{
+				vcount++;
+				i = 1;
+				const char* linec = line.data();
+				for (int k = 0; k < 3; k++) { // k is 0,1,2 for x,y,z
+					while (linec[i] == ' ') i++;
+					j = i;
+					while (linec[j] != ' ') j++;
+					currCood = vlist[vcount][k] = atof(line.substr(i, j - i).c_str());
+					if (firstVertex) 
+						lmin[k] = lmax[k] = currCood;
+					else {
+						if (lmin[k] > currCood)
+							lmin[k] = currCood;
+						if (lmax[k] < currCood)
+							lmax[k] = currCood;
+					}
+					i = j;
+				}
+
+				firstVertex = 0;
+			}
+			if (line[0] == 'f')
+			{
+				tcount++;
+				i = 1;
+				const char* linec = line.data();
+				for (int k = 0; k < 3; k++) {
+					while (linec[i] == ' ') i++;
+					j = i;
+					while (linec[j] != ' ' && linec[j] != '\\') j++;
+					tlist[tcount][k] = atof(line.substr(i, j - i).c_str());
+					i = j;
+					fnlist[tcount][k] = 0;
+					while (linec[j] != ' ') j++;
+
+				}
+
+			}
+
+
+		}
+	}
+
+	// We suggest you to compute the normals here
+
+    cout << "No. of vertices: " << vcount << endl;
+    cout << "No. of triangles: " << tcount << endl;
+    computeStat();
+}
+
+
+
+void myObjType::computeStat()
+{
+	int i;
+    double minAngle = 0;
+    double maxAngle = 0;
+
+    
+    cout << "Min. angle = " << minAngle << endl;
+    cout << "Max. angle = " << maxAngle << endl;
+
+	cout << "Statistics for Maximum Angles" << endl;
+	for (i = 0; i < 18; i++)
+		cout << statMaxAngle[i] << " ";
+	cout << endl;
+	cout << "Statistics for Minimum Angles" << endl;
+	for (i = 0; i < 18; i++)
+		cout << statMinAngle[i] << " ";
+	cout << endl;
+}
